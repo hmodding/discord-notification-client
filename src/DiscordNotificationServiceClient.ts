7f4a8ba57d9cf5e8bf1a042199e70c6a4208c4d8
traxam <traxam@trax.am>
2020-10-27 16:30:06 +0100

    feat: add notification service clientThe service client includes an implementation of the mod versionrelease endpoint.

--- src/DiscordNotificationServiceClient.ts
+++ src/DiscordNotificationServiceClient.ts
@@ -1,6 +1,44 @@
+import post from "axios";
+import { DiscordNotificationServiceError } from "./DiscordNotificationServiceError";
+import { ModVersion } from "./entities/ModVersion";
+
 /**
  * This client allows you to interact with a Discord notification service.
  */
 export class DiscordNotificationServiceClient {
-  
+  private apiBaseUrl: string;
+  private token: string;
+
+  public constructor(apiBaseUrl: string, token: string) {
+    this.apiBaseUrl = apiBaseUrl;
+    this.token = token;
+
+    // remove trailing slash
+    if (this.apiBaseUrl.charAt(this.apiBaseUrl.length - 1) === '/') {
+        this.apiBaseUrl = this.apiBaseUrl.substr(0, this.apiBaseUrl.length - 1);
+    }
+  }
+
+  /**
+   * Send a mod version release notification.
+   * @param version the released mod version.
+   */
+  public async sendModVersionReleaseNotification(version: ModVersion): Promise<void> {
+    const response = await post(this.apiBaseUrl + '/webhooks/mod/version', {
+      data: version,
+      auth: {
+          username: 'user',
+          password: this.token,
+      },
+      method: 'POST',
+    });
+
+    if (response.status !== 200) {
+       if (response.data !== undefined && response.data.error !== undefined) {
+           throw new DiscordNotificationServiceError(response.status, response.data);
+       } else {
+           throw new DiscordNotificationServiceError(response.status);
+       }
+    }
+  }
 }
---
+++ src/DiscordNotificationServiceError.ts
@@ -0,0 +1,28 @@
+/**
+ * A discord notification service error indicates a failed request to the
+ * notification service.
+ */
+export class DiscordNotificationServiceError extends Error {
+  private statusCode: number;
+  
+  /**
+   * Constructs a discord notification service error from an api response.
+   * @param statusCode the http status code of the api response.
+   * @param apiResponse the api response.
+   */
+  public constructor(statusCode: number, apiResponse?: {error: string, message: string}) {
+    if (apiResponse === undefined) {
+        super(`Unknown Discord Notification service error (code ${statusCode})!`);
+    } else {
+        super(`${apiResponse.error}: ${apiResponse.message}`);
+    }
+    this.statusCode = statusCode;
+  }
+
+  /**
+   * @returns the http status code from the service response.
+   */
+  public get getStatusCode() {
+    return this.statusCode;
+  }
+}
